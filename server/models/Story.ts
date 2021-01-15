44def841c762bbc87972ddc889c721898e2df60c
jacksluong <jacksluong@gmail.com>
2021-01-15 12:20:07 -0500

    updated Story model

--- server/models/Story.ts
+++ server/models/Story.ts
@@ -2,20 +2,19 @@ import { Schema, model, Document } from "mongoose";
 
 const StorySchema = new Schema({
   name: String,
-  googleid: String,
   contributors: [String], // user IDs
   content: String,
-  usersThatLiked: [String]
+  usersThatLiked: [String],
+  keywords: [String]
 });
 
 export interface Story extends Document {
   name: string;
-  googleid: string;
   _id: string;
-  wordsTyped: number;
-  storiesWorkedOn: [string]; // story IDs
-  storiesLiked: [string]; // story IDs
-  wordFrequencies: [string: number]; 
+  contributors: [string];
+  content: string;
+  usersThatLiked: [string]; // story IDs
+  keywords: [string]; 
 }
 
 const StoryModel = model<Story>("Story", StorySchema);
