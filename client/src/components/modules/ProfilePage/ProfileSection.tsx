4553f12136cfdb394ea244a1c7ae7f1e275b2c87
Tony Cui <cuitony.baloney@gmail.com>
2021-01-16 21:19:57 -0800

    add blank pfp

--- client/src/components/modules/ProfilePage/ProfileSection.tsx
+++ client/src/components/modules/ProfilePage/ProfileSection.tsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 import "./ProfilePage.css"
-
+import blankpfp from "../../../assets/blankpfp.png"
 class ProfileSection extends Component{
     constructor(props){
         super(props);
@@ -9,6 +9,7 @@ class ProfileSection extends Component{
         return(
             <div className="ProfileSection-container">
                 profile
+                <img src={blankpfp}/>
             </div>
         );
     }
