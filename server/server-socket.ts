a7fe3c96b32bc0d722a58c182ff770750c27a67f
jacksluong <jacksluong@gmail.com>
2021-01-16 21:49:16 -0500

    oops fixed a small thing

--- server/server-socket.ts
+++ server/server-socket.ts
@@ -44,7 +44,7 @@ export const init = (server: http.Server): void => {
       console.log(`socket has disconnected ${socket.id}`);
 
       // TODO: (handle it if they are in a game -> "playerleft")
-      logic.disconnectPlayer(socketToUserMap[socket.id]);
+      logic.disconnectPlayer(socketToUserMap.get(socket.id));
 
       const user = getUserFromSocketID(socket.id);
       if (user !== undefined) removeUser(user, socket);
