a5c3d8d462df1a58910bd37bb00388b992e33478
Tony Cui <cuitony.baloney@gmail.com>
2021-01-22 13:41:37 -0800

    comment interface setup

--- server/models/Story.ts
+++ server/models/Story.ts
@@ -2,8 +2,8 @@ import { Schema, model, Document } from "mongoose";
 
 const Comment = new Schema({
   senderId: String,
-  content: String
-})
+  content: String,
+});
 
 const StorySchema = new Schema({
   name: String,
@@ -12,8 +12,12 @@ const StorySchema = new Schema({
   content: String,
   usersThatLiked: [String],
   keywords: [String],
-  comments: [Comment] 
+  comments: [Comment],
 });
+export interface Comment extends Document {
+  senderId: string;
+  content: string;
+}
 
 export interface Story extends Document {
   name: string;
@@ -23,6 +27,7 @@ export interface Story extends Document {
   content: string;
   usersThatLiked: string[]; // story IDs
   keywords: string[];
+  comments: Comment[];
 }
 
 const StoryModel = model<Story>("Story", StorySchema);
