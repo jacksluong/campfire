b28939fd315a30bbd480d57ad519c2b7e637c4ff
Tony Cui <cuitony.baloney@gmail.com>
2021-01-22 22:38:32 -0800

    merge ok?

--- client/src/components/modules/Landing/CommentsBlock.tsx
+++ client/src/components/modules/Landing/CommentsBlock.tsx
@@ -4,17 +4,34 @@ import Comment from "../../../../../shared/Comment";
 interface Props {
   comments: Comment[];
 }
-class CommentsBlock extends Component<Props, {}> {
+interface State {
+  showComments: boolean;
+}
+class CommentsBlock extends Component<Props, State> {
   constructor(props) {
     super(props);
+    this.state = {
+      showComments: false,
+    };
   }
+  handleShowButtonsClick = (event: HTMLButtonElement) => {};
+
   componentDidMount = () => {
     //get comments
   };
   render() {
     let commentsElement = null;
     commentsElement = this.props.comments.map((comment) => <div>{comment.content}</div>);
-    return <div className="CommmentsBlock">{commentsElement}</div>;
+    return (
+      <div className="comments-container">
+        {/* <br></br> */}
+        {this.state.showComments ? (
+          commentsElement
+        ) : (
+          <button className="CommentButton-container">Show comments</button>
+        )}
+      </div>
+    );
   }
 }
 export default CommentsBlock;
