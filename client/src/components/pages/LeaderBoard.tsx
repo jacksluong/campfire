18b55d5e37623fdbac177bc1c3eaad7934b819b4
Tony Cui <cuitony.baloney@gmail.com>
2021-01-26 00:55:05 -0800

    bubbly explore css, leaderboard skeleton

---
+++ client/src/components/pages/LeaderBoard.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+class LeaderBoard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div> </div>;
+  }
+}
+export default LeaderBoard;
