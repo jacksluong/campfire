f27cd15be9f6787a4d61e5a4172992b41f116d25
Tony Cui <cuitony.baloney@gmail.com>
2021-01-26 15:17:35 -0800

    Added leaderboard to routing

--- client/src/components/pages/LeaderBoard.tsx
+++ client/src/components/pages/LeaderBoard.tsx
@@ -3,16 +3,17 @@ import { RouteComponentProps } from "@reach/router";
 
 interface Props extends RouteComponentProps {
   userId: string;
+  pfp: string;
   handleLogin: any;
   handleLogout: any;
 }
 
-class LeaderBoard extends Component {
+class LeaderBoard extends Component<Props, {}> {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div> </div>;
+    return <div>Leaderboard</div>;
   }
 }
 export default LeaderBoard;
