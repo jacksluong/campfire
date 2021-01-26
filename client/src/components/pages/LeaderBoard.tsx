348c78f6902a730ac2480cc89baeea717c04d79f
Tony Cui <cuitony.baloney@gmail.com>
2021-01-26 15:02:47 -0800

    leaderboard skeleton

--- client/src/components/pages/LeaderBoard.tsx
+++ client/src/components/pages/LeaderBoard.tsx
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import { RouteComponentProps } from "@reach/router";
+
+interface Props extends RouteComponentProps {
+  userId: string;
+  handleLogin: any;
+  handleLogout: any;
+}
+
 class LeaderBoard extends Component {
   constructor(props) {
     super(props);
