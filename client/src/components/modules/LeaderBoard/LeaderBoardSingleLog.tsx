83c81253527bd078b6c7e01cff0ad2f33dce520e
Tony Cui <cuitony.baloney@gmail.com>
2021-01-26 15:30:17 -0800

    leaderboard log skeleton

---
+++ client/src/components/modules/LeaderBoard/LeaderBoardSingleLog.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+class LeaderboardSingleLog extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className="Log"></div>;
+  }
+}
+export default LeaderboardSingleLog;
